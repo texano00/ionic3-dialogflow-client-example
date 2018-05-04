5c668a0a8208ad97dc0b91f6d62950179dc04b34
Yuri <y.bacciarini@quidinfo.it>
2018-05-04 10:03:17 +0200

    Init repository 2

---
+++ src/app/home/home.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Content } from 'ionic-angular';
+import { ApiAiClient } from 'api-ai-javascript';
+import { Message } from './models/message';
+import { FormControl, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild(Content) content: Content;
+  accessToken: string = '20374f4fb81043648d39e5505564f887';
+  client;
+  messages: Message[] = [];
+  messageForm: any;
+  chatBox: any;
+  isLoading: boolean;
+
+  constructor(public platform: Platform, public formBuilder: FormBuilder) {
+    this.chatBox = '';
+
+    this.messageForm = formBuilder.group({
+      message: new FormControl('')
+    });
+
+    this.client = new ApiAiClient({
+      accessToken: this.accessToken
+    });
+  }
+
+  sendMessage(req: string) {
+    if (!req || req === '') {
+      return;
+    }
+    this.messages.push({ from: 'user', text: req });
+    this.isLoading = true;
+
+    this.client
+      .textRequest(req)
+      .then(response => {
+        /* do something */
+        console.log('res');
+        console.log(response);
+        this.messages.push({
+          from: 'bot',
+          text: response.result.fulfillment.speech
+        });
+        this.scrollToBottom();
+        this.isLoading = false;
+      })
+      .catch(error => {
+        /* do something here too */
+        console.log('error');
+        console.log(error);
+      });
+
+    this.chatBox = '';
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      this.content.scrollToBottom();
+    }, 100);
+  }
+}
